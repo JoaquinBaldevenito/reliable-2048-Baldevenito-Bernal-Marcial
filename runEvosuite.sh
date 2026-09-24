f81b9457f3edb40111b3bf6b42647af30a050126
ValenMarcial <valentinhormiga@hotmail.com>
2026-09-24 15:21:06 -0300

    Add support for generating EvoSuite tests for Movement class

--- runEvosuite.sh
+++ runEvosuite.sh
@@ -5,6 +5,7 @@ EVOSUITE_JAR="evosuite-1.0.6.jar"
 EVOSUITE_URL="https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar"
 TARGET_CLASS="ar.edu.unrc.game2048.Board"
 TARGET_CLASS1="ar.edu.unrc.game2048.Cell"
+TARGET_CLASS2="ar.edu.unrc.game2048.Movement"
 SEARCH_BUDGET=60
 
 # Download EvoSuite if not exists
@@ -19,12 +20,17 @@ mvn clean compile
 CLASS_PATH=$(pwd)/target/classes
 
 # Generate tests
-echo "Generating EvoSuite tests Board..."
+echo ""\n" Generating EvoSuite tests Board..."
 java -jar "$EVOSUITE_JAR" -projectCP "$CLASS_PATH" -class $TARGET_CLASS \
     -Dsearch_budget=$SEARCH_BUDGET -Dtest_dir=src/test/java -Duse_separate_classloader=false
 
-echo "Generating EvoSuite tests Cell..."
+echo ""\n" Generating EvoSuite tests Cell..."
 java -jar "$EVOSUITE_JAR" -projectCP "$CLASS_PATH" -class $TARGET_CLASS1 \
+    -Dsearch_budget=$SEARCH_BUDGET -Dtest_dir=src/test/java -Duse_separate_classloader=false
+
+
+echo ""\n" Generating EvoSuite tests Movement..."
+java -jar "$EVOSUITE_JAR" -projectCP "$CLASS_PATH" -class $TARGET_CLASS2 \
     -Dsearch_budget=$SEARCH_BUDGET -Dtest_dir=src/test/java -Duse_separate_classloader=false
 # Run tests
 mvn test -Pevosuite
