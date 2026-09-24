8b87ff80b5573abf3a1a4b10f440ab9649c15ca9
ValenMarcial <valentinhormiga@hotmail.com>
2026-09-24 01:14:21 -0300

    Update runEvosuite.sh to correct target class definitions and enhance test generation output

--- runEvosuite.sh
+++ runEvosuite.sh
@@ -3,7 +3,8 @@
 
 EVOSUITE_JAR="evosuite-1.0.6.jar"
 EVOSUITE_URL="https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar"
-TARGET_CLASS="ar.edu.unrc.game2048.Cell"
+TARGET_CLASS="ar.edu.unrc.game2048.Board"
+TARGET_CLASS1="ar.edu.unrc.game2048.Cell"
 SEARCH_BUDGET=60
 
 # Download EvoSuite if not exists
@@ -18,9 +19,12 @@ mvn clean compile
 CLASS_PATH=$(pwd)/target/classes
 
 # Generate tests
-echo "Generating EvoSuite tests..."
+echo "Generating EvoSuite tests Board..."
 java -jar "$EVOSUITE_JAR" -projectCP "$CLASS_PATH" -class $TARGET_CLASS \
     -Dsearch_budget=$SEARCH_BUDGET -Dtest_dir=src/test/java
 
+echo "Generating EvoSuite tests Cell..."
+java -jar "$EVOSUITE_JAR" -projectCP "$CLASS_PATH" -class $TARGET_CLASS1 \
+    -Dsearch_budget=$SEARCH_BUDGET -Dtest_dir=src/test/java
 # Run tests
-mvn test
+mvn test -Pevosuite
